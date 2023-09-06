172c228a2b309a8806d0be1bccbc367a8492d841
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-09-05 18:05:10 -0700

    Update Counting-duplicates.js

--- Counting-duplicates.js
+++ Counting-duplicates.js
@@ -24,3 +24,17 @@ function duplicateCount(text) {
 }
 
 // or
+
+function duplicateCount(text) {
+  let lower = text.toLowerCase();
+  let count = 0;
+  let used = [];
+
+  lower.split('').forEach(function(letter) {
+    if (!used.includes(letter) && (lower.split(letter).length - 1) > 1) {
+      count++;
+      used.push(letter);  
+    }
+  });
+  return count;
+}
