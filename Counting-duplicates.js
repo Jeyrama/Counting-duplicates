aedd828f8396adbe34e7a3e5a64b09c682db9448
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-09-05 17:59:38 -0700

    Update Counting-duplicates.js

--- Counting-duplicates.js
+++ Counting-duplicates.js
@@ -16,3 +16,11 @@ Example:
 
 
 // Solution
+
+function duplicateCount(text) {
+  return text.toLowerCase().split('').filter(function(val, i, arr) {
+    return arr.indexOf(val) !== i && arr.lastIndexOf(val) === i;
+  }).length;
+}
+
+// or
